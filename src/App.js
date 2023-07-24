b09601f10e8d1962712b7e7b08f50c0c4bff66cd
Yashchovatiya11 <welcome80808@gmail.com>
2023-07-24 09:08:58 +0530

    api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-unused-vars */
+import { Routes, Route } from "react-router-dom"
+import User from './Componants/User'
+import Profile from './Componants/Profile'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={ <User/> } />
+        <Route path="/Profile/:id/:name" element={ <Profile/> } />
+      </Routes>
     </div>
-  );
+  )
 }
 
 export default App;
